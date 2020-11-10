d15b7df909538ecf6f81dd8b7374f3f88b2dadae
Arash <arash9_shafei8@outlook.com>
2020-11-10 07:15:00 -0500

    Homepage

---
+++ Homepage/slideshow.js
@@ -0,0 +1,29 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) 
+{
+    document.getElementById("promo").style.display="none";
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) 
+{
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  
+}
