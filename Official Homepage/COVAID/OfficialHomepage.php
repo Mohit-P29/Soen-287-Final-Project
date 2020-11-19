e5b9da75b9de34fbcd737ce68d03c952681c3ca0
Arash <arash9_shafei8@outlook.com>
2020-11-18 23:17:32 -0500

    Homepage V.2.0
    1. Deleted all old files
    2. Pushed all new files

---
+++ Official Homepage/COVAID/OfficialHomepage.php
@@ -0,0 +1,149 @@
+<?php
+include('includes/header.php')
+    ?>
+
+<main>
+            <br />
+            <br />
+            <!---------------- Slideshow ------------------->
+            <div class="slideshow-container">
+                <div class="mySlides fade">
+                    <img src="images/maskSlide.jpg" width="100%" height="500px;">
+                    <div class="slidetext"> 
+                        <p>
+                            Get Your Protection Masks Now!
+                        </p>
+                        <button class="slidebutton">Shop</button>
+                    </div>
+                </div>
+                <div class="mySlides fade">
+                    <img src="images/DIYmaskSlide.jpg" width="100%" height="500px;">
+                     <div class="slidetext1"> 
+                        <p>
+                            Explore with Custom Protection Masks!
+                        </p>
+                        <button class="slidebutton">Shop</button>
+                    </div>
+                </div>
+                <div class="mySlides fade">
+                    <img src="images/sanitizerSlide.jpg" width="100%" height="500px;">
+                     <div class="slidetext"> 
+                        <p>
+                             Premium Hand Sanitizer Avaiable Now!
+                        </p>
+                        <button class="slidebutton">Shop</button>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div style="text-align:center">
+              <span class="dot"></span> 
+              <span class="dot"></span> 
+              <span class="dot"></span> 
+            </div>
+            <script type="text/javascript" src="index2.js"></script>
+            <br />
+            <br />
+            <br />
+            
+            <!------------------ Top Sellers Section ----------------->
+            <div class="topsellers"> 
+                <p class="topsellers_heading"><strong>Top Sellers</strong> </p>
+                <br />
+                <div class="product_list">
+                    <div class="product">
+                        <a href ="#"><img src="images/mask.jpg" alt="Mask" width="150px" height="150px"></a>
+                        <p class="topsellers_text"> 50-PIECE 4-Ply Disposable Face Masks </p>
+                        <p class="topsellers_text1"> Sizes: Kids &amp; Adults </p>
+                        <br />
+                        <p class="topsellers_text"> $19.99 CAD </p>
+                        <div class="topsellers_button">
+                            <a href="#">Shop</a>
+                        </div>
+                    </div>
+                    <div class="product">
+                        <a href ="#"><img src="images/purell.jpg" alt="Hand Sanitizer" width="150px" height="150px"></a>
+                        <p class="topsellers_text"> Purell Advanced Hand Rub Sanitizer Gel </p>
+                        <p class="topsellers_text1"> Format: 256mL </p>
+                        <br />
+                        <p class="topsellers_text"> $6.99 CAD </p>
+                        <div class="topsellers_button">
+                            <a href="#">Shop</a>
+                        </div>
+                    </div>
+                    <div class="product">
+                        <a href ="#"><img src="images/lysol.jpg" alt="Lysol Wipes" width="150px" height="150px"></a>
+                        <p class="topsellers_text"> Lysol Disinfectant Wipes - Multipurpose Wipes </p>
+                        <p class="topsellers_text1"> Format: 80x sheets </p>
+                        <br />
+                        <p class="topsellers_text"> $6.99 CAD </p>
+                        <div class="topsellers_button">
+                            <a href="#">Shop</a>
+                        </div>
+                    </div>
+                    <div class="product">
+                        <a href ="#"><img src="images/handsanitizerMINI.jpg" alt="Small Hand Sanitizer" width="150px" height="150px"></a>
+                        <p class="topsellers_text"> Purell-Mini Advanced Hand Rub Sanitizer Gel </p>
+                        <p class="topsellers_text1"> Format: 59mL </p>
+                        <br />
+                        <p class="topsellers_text"> $3.99 CAD </p>
+                        <div class="topsellers_button">
+                            <a href="#">Shop</a>
+                        </div>
+                    </div>
+                    <div class="product">
+                        <a href ="#"><img src="images/faceshield.jpg" alt="Face shield" width="150px" height="150px"></a>
+                        <p class="topsellers_text"> 5 PIECE - Resusable Face Shield </p>
+                        <p class="topsellers_text1"> Format: 5 Units </p>
+                        <br />
+                        <p class="topsellers_text"> $19.99 CAD </p>
+                        <div class="topsellers_button">
+                            <a href="#">Shop</a>
+                        </div>
+                    </div>
+                    <div class="product">
+                        <a href ="#"><img src="images/lysolspray.jpg" alt="Lysol Spray" width="150px" height="150px"></a>
+                        <p class="topsellers_text"> Lysol Disinfectant Spray, Crisp Linen Scent </p>
+                        <p class="topsellers_text1"> Format: 19oz </p>
+                        <br />
+                        <p class="topsellers_text"> $14.99 CAD </p>
+                        <div class="topsellers_button">
+                            <a href="#">Shop</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            
+            <!------------------ Donate Video Section --------------------->
+            
+            <div class="video">
+            <div class="video_container">
+                <img src="images/redcross.jpg" alt="Red Cross Logo" height="200px" width="400px">
+                <div class="redcross_text">
+                    <p> Consider Donating to Canadian Red Cross. </p>
+                    <p> Learn about Canadian Red Cross' goals in the coming future.</p> 
+                    <br />
+                    <br />
+                    <div class="donate_buttons"><p class="video_button"><a href="#">Learn more</a></p>
+                    <p class="video_button"><a href="#">Donate now</a></p></div>
+                </div>
+            </div>
+            <div class="redcross_video">
+                <video width="700px" height="400px" controls><source src="Video/RedCross.mp4" type="video/mp4"></video>
+            </div>
+            </div>
+            <br />
+            <br />
+            <br />
+        </main>
+
+<?php 
+    include('includes/footer.php')
+    ?>
+
