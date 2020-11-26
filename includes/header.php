ca6c5d74a4a6c822ce2ee506baba76da0680d5e5
Mohit-P29 <Mohit_Patel299@hotmail.com>
2020-11-25 19:17:58 -0500

    Big mask update
    removed old mask files, addded new mask php files, and an animals image file

---
+++ includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>COV-AID | Welcome!</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="mask_style.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </head>
+    
+    <body>
+        
+        <!----------- HEADER => The Navigation bar that will be reproduced on all pages of the website ----------------->
+        <header> 
+            <a class="logo" href="#"><img src="images/CompLogo.jpg" alt="logo" height="100px" width="100px"></a>
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="#"><img src="images/cart.jpg" alt="My Cart" height="25px" width="25px"></a>
+            <a class="cta" href="#"><img src="images/signin.jpg" alt="Sign In" height="25px" width="25px"></a>
+            <p class="menu cta">Menu</p>
+        </header>
+        <div class="overlay">
+            <a class="close">&times;</a>
+            <div class="overlay__content">
+                <a href="#">Home</a>
+                <a href="#">Shop</a>
+                <a href="#">About Us</a>
+                <a href="#">Contact Us</a>
+                <a href="#"><img src="" alt="" height="50px" width="50px"></a>
+                <a href="#"><img src="" alt="" height="50px" width="50px"></a>
+            </div>
+        </div>
+        <script type="text/javascript" src="index.js"></script>
+         <script type="text/javascript" src="/php_projects/Soen_project/Soen-287-Final-Project/js/mask.js"></script>
