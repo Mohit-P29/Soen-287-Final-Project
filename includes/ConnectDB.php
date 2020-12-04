e1be78016321ae4c07665761f01dcb87e5196c59
Kayi626 <jiawyao@hotmail.com>
2020-12-03 23:50:09 -0500

    <UserPage>

--- includes/ConnectDB.php
+++ includes/ConnectDB.php
@@ -1,8 +1,8 @@
 <?php
 $db_servername="localhost";
 $db_username="root";
-$db_password="database11";
-$db_name="user";
+$db_password="";
+$db_name="covaid_store";
 
 $conn=mysqli_connect($db_servername,$db_username,$db_password,$db_name);
 
