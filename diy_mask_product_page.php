360a8df7adfb62879520a1d9e991ac3e9eca2c92
Mohit-P29 <Mohit_Patel299@hotmail.com>
2020-12-05 21:21:34 -0500

    mask page css

--- diy_mask_product_page.php
+++ diy_mask_product_page.php
@@ -275,6 +275,52 @@ if(selectedValue == 1){
 }
     
 }
+	
+	
+	
+	//Make the DIV element draggagle:
+dragElement(document.getElementById("image"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "-location")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "-location").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
     
     </script>
     
@@ -294,6 +340,7 @@ if(selectedValue == 1){
     
     
 </select>
+<br> <br>
 
 
  <label class="label_op"><strong>Colors Outline</strong></label> 
@@ -309,7 +356,10 @@ if(selectedValue == 1){
     
     
 </select>
-       <br> <br>
+       <br> <br><br><br><br> <br><br><br><br> <br><br><br>
+       
+       <p>Add an Image to your mask from our selection</p>
+       <br> <br><br><br>
        <label class="label_i"><strong>Images</strong></label>
 <select id="selection_box3" name="images" class="image-select">
     <option value="1" data-picture="animal/elephant.png" >Elephant</option>
@@ -339,12 +389,18 @@ if(selectedValue == 1){
      
      
   </script>
+  <br><br><br><br><br><br><br><br><br>
 
+<div id="image">
 
 <div id="image-location" class="picture_container">
     
+    
+    
+    
    
     
+</div>
 </div>
 
 <input class='btn' type="submit" value="Add to Cart">
