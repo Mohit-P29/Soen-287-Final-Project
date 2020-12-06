5ad1393818f1579787c7e430f2d526e076cdf2f0
Mohit-P29 <Mohit_Patel299@hotmail.com>
2020-12-06 16:33:26 -0500

    added table inside mask checkout

--- mask_checkout.php
+++ mask_checkout.php
@@ -7,11 +7,11 @@ require_once 'connection/check_connection.php';
 
 ?>
 
-
+<h1> Dummy DIY Mask Builder Cart</h1>
 <?php
 
 
-// updating instead of inserting mask
+// fetching data from the previous page that the user selects
 if( !isset($_POST["delete"])){
 
  $colorP = $_POST['mask_primary_color'];
@@ -23,19 +23,8 @@ echo $colorS;
 echo $imagesF;
 
 
-/*
 
-$sql = "UPDATE masks SET primary_color_Id='$colorP', secondary_color_Id='$colorS', image_Id='$imagesF'   WHERE maskId=1";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Record updated successfully";
-} else {
-  echo "Error updating record: " . $conn->error;
-}
-
-
-*/
-
+// inserting new mask that the user would like to add to the cart
 
   $sql = "INSERT INTO masks (primary_color_Id, secondary_color_Id, image_Id)
 VALUES ('$colorP', '$colorS', '$imagesF')";
@@ -53,12 +42,23 @@ if ($conn->query($sql) === TRUE) {
 $newMaskId = mysqli_insert_id($conn);
 
 
-
 echo "Newly add Mask id: ".$newMaskId."<br>";
 }
 	
 
+//----------------------This is where the mask table is displayed-------------------------
+?>
+<!-----------------HTML-------------------->
 
+<div style="border: 1px solid black; width:1500px; border-radius: 25px; padding: 25px;border-width: 10px;" >
+
+
+
+<?php
+
+
+
+// setting up my select query
 $sql = "SELECT maskId, u1.color_name primary_color_name, u2.color_name secondary_color_name, images.image_name 
 FROM masks 
 JOIN colors u1 ON masks.primary_color_Id = u1.colorId 
@@ -67,43 +67,66 @@ JOIN images ON masks.image_Id = images.image_id";
 
 $result = $conn->query($sql);
 
-// 1. attempt ash
-/* if ($result->num_rows > 0) {
-  echo "<table><tr><th>ID</th><th>Name</th></tr>";
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "<tr><td>".$row["maskId"]."</td><td>".$row["primary_color_Id"]." ".$row["secondary_color_Id"]."</td></tr>";
-  }
-  echo "</table>";
-} else {
-  echo "0 results";
-} */
 
-//2. attempt mohit stack overflow
 ?>
+
+<!--- where my table is going to be displayed --->
 
 <table style="width:40%; border: 1px solid black;">
 
-<?php
+ <tr>
+<th style="border: 1px solid black;">
+Product Id
+</th>
+<th style="border: 1px solid black;">
+Mask Main Color
+</th>
+<th style="border: 1px solid black;">
+Mask Outline Color
+</th>
+<th style="border: 1px solid black;">
+Images
+</th>
+
+<?php // BEGINNING OF PHP TAG
+	
+	
 echo "These are the masks in your cart at the moment:"."<br>";
 	
 
 	
 if (!empty($result) && $result->num_rows > 0) {
-  // output data of each row
+	
+  // output data from each row of the database into each row of the table
+	
+	
   while($row = $result->fetch_assoc()) {
+	  
+	  //END OF PHP TAG
 	  ?>
 	  <tr>
-    <th>
+    <th style="border: 1px solid black;">
 	  
 	   <?php
-    echo "<br>" ."maskId: " . $row["maskId"].  " Color_Primary: " . $row["primary_color_name"]. " Color_Outline:" . $row["secondary_color_name"]. " Image: " . $row["image_name"]. "<br>";
-	?>
+    echo "<br>" ."maskId:  " . $row["maskId"];  ?>
 	  </th>
+	   <th style="border: 1px solid black;">
+	  <?php
+	echo " Color_Primary:  " . $row["primary_color_name"]; ?>
+		</th>
+		 <th style="border: 1px solid black;">
+		<?php
+	echo " Color_Outline: " . $row["secondary_color_name"]; ?>
+		</th>
+		 <th style="border: 1px solid black;">
+		<?php
+	echo " Image: " . $row["image_name"]. "<br>"; ?>
+		</th>
+	  
 	  <?php
   }
 } else {
-  echo "0 results";
+  echo "You Have No masks in your Cart";
 }
 
 
@@ -111,7 +134,7 @@ if (!empty($result) && $result->num_rows > 0) {
 
 
 ?>
-
+</tr>
 </table>
 
 	
@@ -119,7 +142,7 @@ if (!empty($result) && $result->num_rows > 0) {
 	
 	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 	
-		<p>If you'd like to delete a mask please enter its mask Id number here and press delete</p>
+		<p>If you'd like to delete a mask please enter its mask Id number under here and confirm, then once confirmed you can delete: </p>
 	<input type="text" value="" name="maskToDelete" placeholder="MaskId">
 	<input  class="submit" type="submit" name="delete" value="Confirm" />
 	
@@ -150,6 +173,7 @@ $result = $conn->query($sql);
 		
 	  if(isset($_POST["delete"]))
         {
+		  $deleteMask = false;
 		  
 	if (!empty($result) && $result->num_rows > 0) {
 		
@@ -167,6 +191,7 @@ $result = $conn->query($sql);
 		 $sql ="DELETE FROM masks WHERE maskId='$maskDeleted' ";
 		  if ($conn->query($sql) === TRUE) {
   echo "New Id Loaded successfully, press delete to remove from cart";
+		$deleteMask = true;	  
 	
 	
 } else {
@@ -176,7 +201,7 @@ $result = $conn->query($sql);
 		  
 	  }else{
 		  
-		  echo "mask number entered is not there";
+		 
 		 
 	  }
 	  
@@ -186,6 +211,16 @@ $result = $conn->query($sql);
   echo "0 results";
 }
 	  }
+			
+			
+	if($deleteMask == false){
+		 echo "Mask you are searchin for is not in your cart";
+		
+	}		
+			
+			
+			
+			
 		}
 		
 	$conn->close();
@@ -194,6 +229,8 @@ $result = $conn->query($sql);
 
 	<input  class="submit" type="submit" name="delete" value="Delete" />
 	</form>
+	
+	</div>
 		<form action="diy_mask_product_page.php" method="post">
 		
 		<input  class="submit" type="submit" name="submit" value="Goback to mask builder" />
