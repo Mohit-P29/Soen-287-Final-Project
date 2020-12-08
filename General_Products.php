d30780c8183d757aea17635866048f7343678d1c
Amrfaris <73029994+Amrfaris@users.noreply.github.com>
2020-12-08 03:48:30 +0200

    General Product page (dynamic)

--- General_Products.php
+++ General_Products.php
@@ -1,173 +1,154 @@
 
-    <?php 
-    include("includes/header.php");
-    ?>
-    <link rel="stylesheet" href="css/product_general.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
-    <!----featured products-->
-    <div class= "submenu">
-      <div class="productRow">
-        <h2>General Products</h2>
-        <select>
-          <option >Recommended</option>
-          <option >Sort by Price</option>
-          <option >Newest Items</option>
-          <option >Sort by Rating</option>
-          <option >Most bought Products</option>
-        </select>
-      </div>
-      <div class="defaultRow">
-        <div class="single-products">
-          <img src="products/product 1/p1m.jpg" style="width:50%;">
-          <h4>Jermee Moisturizing Hand Sanitizer Gel</h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-          </div>
-          <p>$29.95</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 2/p2m.jpg">
-          <h4>Travel Clean KIT (2-Pack) </h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-half-alt-solid.svg">
-          </div>
-          <p>$34.95</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 3/p3m.jpg">
-          <h4>Sterillium Hand Sanitizer</h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-regular.svg">
-            <img src="images/Icons/star-regular.svg">
-          </div>
-          <p>$2.89</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 4/p4m.jpg">
-          <h4>FFP2 Mask (without Valve) </h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-          </div>
-          <p>$6.95</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 5/p5m.jpg">
-          <h4>Teqler Surgical Mask, black</h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-          </div>
-          <p>$29.99</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 6/p6m.jpg">
-          <h4>Disposable PE Gloves</h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-half-alt-solid.svg">
-            <img src="images/Icons/star-regular.svg">
-            <img src="images/Icons/star-regular.svg">
-          </div>
-          <p>$4.30</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 7/p7m.jpg">
-          <h4>Disposable Overall</h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-half-alt-solid.svg">
-          </div>
-          <p>$18.99</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 8/p8m.jpg">
-          <h4>Disposable Gown (PP fleece)  </h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-half-alt-solid.svg">
-            <img src="images/Icons/star-regular.svg">
-          </div>
-          <p>$14.99</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 9/p9m.jpg">
-          <h4>Clorox Disinfecting Wipes</h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-half-alt-solid.svg">
-          </div>
-          <p>$7.99</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 10/p10m.jpg">
-          <h4>Purell ES8 disinfectant dispenser</h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-regular.svg">
-            <img src="images/Icons/star-regular.svg">
-            <img src="images/Icons/star-regular.svg">
-          </div>
-          <p>$38.85</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 11/p11m.png">
-          <h4>octenisan Face Cloth</h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-regular.svg">
-          </div>
-          <p>$4.99</p>
-        </div>
-        <div class="single-products">
-          <img src="products/product 12/p12m.jpg" style="width:150%;">
-          <h4>Wellisair Air Disinfection Purifier </h4>
-          <div class="rating">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-solid.svg">
-            <img src="images/Icons/star-half-alt-solid.svg">
-          </div>
-          <p>$782</p>
-        </div>
-      </div>
-    </div>
+
+<?php
+  $page_title = 'All Products';
+   include("includes/header.php");
+   include_once "includes/covaid_database.php";
    
-    <!----footer-->
-    <?php 
-    include("includes/footer.php");
-    ?>
-  </body>
-</html>
+   //Retrieve all products from the database
+   $product_name;
+   $product_price;
+   $total_sales;
+   $product_inventory;
+   $number_reviews = 0; //Unknown
+   $average_review = 0; //Unknown
+   $adminlink;
+
+
+   if(isset($_GET['order'])){
+    $order=$_GET['order'];
+  }else{
+    $order= 'created';
+  }
+  if(isset($_GET['sort'])){
+   $sort=$_GET['sort'];
+ }else{
+   $sort= 'DESC';
+ }
+   
+   $sql = "SELECT * FROM products ORDER BY $order $sort;";
+   $result = mysqli_query($conn, $sql);
+   $resultCheck = mysqli_num_rows($result);
+
+
+ 
+  
+ 
+
+  
+   //Makes sure that the connection was established
+   if ($resultCheck > 0) {
+
+    if ($sort== 'ASC'){
+
+      $sort='DESC';
+    }else{
+      $sort='ASC';
+    }
+    
+     echo " <main>
+    <link rel='stylesheet' href='css/product_general.css'>
+    <link rel='preconnect' href='https://fonts.gstatic.com'>
+    <link href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap' rel='stylesheet'>
+ 
+    <!----featured products-->
+    <div class= 'submenu'>
+       <div class='productRow'>
+          <h2>General Products</h2>
+          <a href ='?order=name&&sort=$sort'>Sort by Name</a>
+          <a href ='?order=price&&sort=$sort'>Sort by Price</a>
+          <a href ='?order=created&&sort=$sort'>Newest Items</a>
+          <a href ='?order=sales&&sort=$sort'>Most Bought Products</a>
+          
+       </div>
+       <div class='defaultRow'>" ;
+       
+ 
+       while ($row = mysqli_fetch_assoc($result)) {
+           $product_id = $row['id'];
+           $product_name = $row['name'];
+           $product_price = (float) $row['price'];
+           $total_sales = (float) $row['sales'];
+           $product_inventory = (float) $row['inventory'];
+           $productlink = $row['webpageLink'];
+
+      
+           $imagelink= $row['image1']; 
+           if(!isset($imagelink)|| $imagelink==''){
+            $imagelink='image/products/noimage.jpg';
+           }
+
+           echo " 
+           <div class='single-products'>
+           <a href='$productlink'> <img src='$imagelink' style='width:50%;
+            display: block;
+         margin-left: auto;
+         margin-right: auto;'></a>
+         </br>
+            <h4><a href='$productlink'>$product_name</a></h4>
+            <div class='rating'>";
+       
+            $number_reviews = 0; //Unknown
+            $average_review = 0; //Unknown
+            $sql1 = "SELECT COUNT(num_star) AS num_reviews FROM reviews WHERE product_id = $product_id;";
+            $result1 = mysqli_query($conn, $sql1);
+            $resultCheck1 = mysqli_num_rows($result1);
+            if ($resultCheck1 > 0) {
+               $row1 = mysqli_fetch_assoc($result1);
+               $number_reviews = $row1['num_reviews'];
+            }
+            if ($number_reviews != 0) {
+               $sql2 = "SELECT SUM(num_star) AS star_sum FROM reviews WHERE product_id = $product_id;";
+               $result2 = mysqli_query($conn, $sql2);
+               $resultCheck2 = mysqli_num_rows($result2);
+               if ($resultCheck2 > 0) {
+                  $row2 = mysqli_fetch_assoc($result2);
+                  $sum_reviews = $row2['star_sum'];
+                  $average_review = $sum_reviews / $number_reviews;
+               }
+            }
+            $count = 5;
+            while($count > 0){
+              if($average_review >= 1){
+                echo "<img src='images/Icons/star-solid.svg'>";
+              }else if($average_review > 0){
+                echo "<img src='images/Icons/star-half-alt-solid.svg'>";
+              }else{
+                echo "<img src='images/Icons/star-regular.svg'>";
+              }
+              $average_review -= 1;
+              $count -= 1;
+            }
+
+        
+              
+           echo" </div>
+            <p>$$product_price</p>
+         </div>
+           ";
+   
+          
+           
+   
+   
+          
+       }
+       echo "</div>
+       </div>
+    </main>
+    <!----footer-->";
+    
+       include("includes/footer.php");
+     echo"
+    </body>
+    </html> ";
+   }
+   else{
+    echo "no data avalaible";
+  }
+   ?>
+
+
+
+
+
